4016cc05801e31c7b362b4aa7e8ba744c731efab
Greg Mathis <gmathis@panopto.com>
2011-09-22 07:39:12 +0800

    Restore single course provisioning functionality.

--- edit_form.php
+++ edit_form.php
@@ -12,12 +12,17 @@ class block_panopto_edit_form extends block_edit_form {
     {
       $mform->addElement('header', 'configheader', get_string('block_edit_header', 'block_panopto'));
   
-      $params->course_ids = $COURSE->id;
-			$params->return_url = urlencode($_SERVER['REQUEST_URI']);
-			$query_string = http_build_query($params);
-			$provision_url = "$CFG->wwwroot/blocks/panopto/provision_course.php?" . $query_string;
-			$course_list = $panopto_data->get_course_options($provision_url);
-      $mform->addElement('selectgroups', 'config_course', get_string('course', 'block_panopto'), $course_list['courses']);
+      $params->course_id = $COURSE->id;
+      $params->return_url = $_SERVER['REQUEST_URI'];
+      $query_string = http_build_query($params, '', '&');
+      $provision_url = "$CFG->wwwroot/blocks/panopto/provision_course.php?" . $query_string;
+
+      $add_to_panopto = get_string('add_to_panopto', 'block_panopto');
+      $or = get_string('or', 'block_panopto');
+      $mform->addElement('html', "<a href='$provision_url'>$add_to_panopto</a><br><br>-- $or --<br><br>");
+      
+      $course_list = $panopto_data->get_course_options();
+      $mform->addElement('selectgroups', 'config_course', get_string('existing_course', 'block_panopto'), $course_list['courses']);
       $mform->setDefault('config_course', $course_list['selected']);
     } else {
       $mform->addElement('static', 'error', '', get_string('block_edit_error', 'block_panopto'));
