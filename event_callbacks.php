3aaf6c3eee059b3e024a8b1ace89fcee29c25725
Spenser Jones <spenser.e.jones@gmail.com>
2011-11-07 16:04:25 -0700

    Disabled event callbacks while leaving the code there, for future modification

--- event_callbacks.php
+++ event_callbacks.php
@@ -4,7 +4,7 @@ require_once('lib/panopto_data.php');
 // Called by event handlers in Moodle 2
 // Written by Spenser Jones (http://spenserjones.com) on behalf of Ambrose University College
 function sync_users($ra) {
-  print_r($ra);
+  /*print_r($ra);
   $context = get_context_instance_by_id($ra->contextid);
   print_r($context);
   $panopto_data = new panopto_data(null);
@@ -15,5 +15,6 @@ function sync_users($ra) {
   print_r($provisioning_data);
   $provisioned_data = $panopto_data->provision_course($provisioning_data);
   print_r($provisioned_data);
-  return !empty($provisioned_data);
+  return !empty($provisioned_data);*/
+  return true;
 }
