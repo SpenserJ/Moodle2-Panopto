371230114f99f21570f16d068207082c457b8d68
Panopto Dev <jlin@panopto.com>
2013-07-09 10:43:20 -0700

    Update version to use the right format
    The optional version number had the wrong format based on this Moodle
    guidance:
    http://docs.moodle.org/dev/version.php

--- version.php
+++ version.php
@@ -35,7 +35,7 @@
 defined('MOODLE_INTERNAL') || die();
  
 $plugin->version   = 2013061901;
-$plugin->requires  = 1.9.0;
+$plugin->requires  = 2010112400; // Version 2.0
 $plugin->cron      = 0;
 $plugin->maturity  = MATURITY_STABLE;
  
