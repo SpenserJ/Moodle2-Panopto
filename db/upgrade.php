31e69d5d06f25b3d1f40f99e93b488b25027a362
Peter Lancaster <peterlancast@gmail.com>
2014-12-29 16:54:07 -0800

    Previously provisioned courses will remained provisioned on upgrade
    Courses provisioned before upgrade will now remain provisioned to the
    same server and will not require a click to reprovision from the course
    page after upgrading.

--- db/upgrade.php
+++ db/upgrade.php
@@ -2,16 +2,28 @@
 function xmldb_block_panopto_upgrade($oldversion = 0) {
     global $CFG, $DB;
     $dbman = $DB->get_manager();
+    
+
+
     if ($oldversion < 2014121502) {
 
+        if(isset($CFG->block_panopto_server_name)){
+            $oldServerName = $CFG->block_panopto_server_name;
+        }
+        if(isset($CFG->block_panopto_application_key)){
+            $oldAppKey = $CFG->block_panopto_application_key;
+        }
+        
         // Define field panopto_server to be added to block_panopto_foldermap.
         $table = new xmldb_table('block_panopto_foldermap');
         $field = new xmldb_field('panopto_server', XMLDB_TYPE_CHAR, '64', null, XMLDB_NOTNULL, null, null, 'panopto_id');
-
+        
         // Conditionally launch add field panopto_server.
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
-        }  
+            $DB->set_field('block_panopto_foldermap', 'panopto_server', $oldServerName, null);
+        }
+                  
 
         // Define field panopto_app_key to be added to block_panopto_foldermap.
         $table = new xmldb_table('block_panopto_foldermap');
@@ -20,8 +32,9 @@ function xmldb_block_panopto_upgrade($oldversion = 0) {
         // Conditionally launch add field panopto_app_key.
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
+            $DB->set_field('block_panopto_foldermap', 'panopto_app_key', $oldAppKey, null);
         }
-
+       
         // Panopto savepoint reached.
         upgrade_block_savepoint(true, 2014121502, 'panopto');
     }
