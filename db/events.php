24d6554551f3a2400c8ec72ffb1caef894f1e8d7
Panopto, Inc. <jlin@panopto.com>
2015-01-13 10:21:39 -0800

    Cleanup

--- db/events.php
+++ db/events.php
@@ -4,33 +4,34 @@ defined('MOODLE_INTERNAL') || die();
 $observers = array(
         
         //User enrolled event
-		array(
-			'eventname' => '\core\event\user_enrolment_created',
-			'callback'  =>  'block_panopto_rollingsync::enrolmentcreated',
-		),
+	array(
+		'eventname' => '\core\event\user_enrolment_created',
+		'callback'  =>  'block_panopto_rollingsync::enrolmentcreated',
+	),
 
-		//User unenrolled event
+	//User unenrolled event
         array(
-			'eventname' => '\core\event\user_enrolment_deleted',
-			'callback'  =>  'block_panopto_rollingsync::enrolmentdeleted',
-		),
+		'eventname' => '\core\event\user_enrolment_deleted',
+		'callback'  =>  'block_panopto_rollingsync::enrolmentdeleted',
+	),
 
-		//User's enrollment updated
+	//User's enrollment updated
         array(
-			'eventname' => '\core\event\user_enrolment_updated',
-			'callback'  =>  'block_panopto_rollingsync::enrolmentupdated',
-		),
+		'eventname' => '\core\event\user_enrolment_updated',
+		'callback'  =>  'block_panopto_rollingsync::enrolmentupdated',
+	),
 		
         //Event when user has role added to enrollment
         array(
-			'eventname' => '\core\event\role_assigned',
-			'callback'  =>  'block_panopto_rollingsync::roleadded',
-		),
+		'eventname' => '\core\event\role_assigned',
+		'callback'  =>  'block_panopto_rollingsync::roleadded',
+	),
 
-		//Event when user has role removed from enrollment
+	//Event when user has role removed from enrollment
         array(
-			'eventname' => '\core\event\role_unassigned',
-			'callback'  =>  'block_panopto_rollingsync::roledeleted',
-		),   
-	);
+		'eventname' => '\core\event\role_unassigned',
+		'callback'  =>  'block_panopto_rollingsync::roledeleted',
+	),   
+);
+
 ?>
