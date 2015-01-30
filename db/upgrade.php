cb12ff6ac2ff047ef7dbbc1440f6e15435d3058d
Peter Lancaster <peterlancast@gmail.com>
2015-01-30 11:30:43 -0800

    Make role fields nullable
    This allows no Moodle role to be assigned to a Panopto role

--- db/upgrade.php
+++ db/upgrade.php
@@ -46,23 +46,24 @@ function xmldb_block_panopto_upgrade($oldversion = 0) {
 
         if ($oldversion < 2015012901) {
 
-         // Define field publisher_mapping to be added to block_panopto_foldermap.
+                // Define field publisher_mapping to be added to block_panopto_foldermap.
         $table = new xmldb_table('block_panopto_foldermap');
-        $field = new xmldb_field('publisher_mapping', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, '1', 'panopto_app_key');
+        $field = new xmldb_field('publisher_mapping', XMLDB_TYPE_CHAR, '20', null, null, null, '1', 'panopto_app_key');
 
         // Conditionally launch add field publisher_mapping.
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
-      
+
         // Define field creator_mapping to be added to block_panopto_foldermap.
         $table = new xmldb_table('block_panopto_foldermap');
-        $field = new xmldb_field('creator_mapping', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, '3,4', 'publisher_mapping');
+        $field = new xmldb_field('creator_mapping', XMLDB_TYPE_CHAR, '20', null, null, null, '3,4', 'publisher_mapping');
 
         // Conditionally launch add field creator_mapping.
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
+
 
         // Panopto savepoint reached.
         upgrade_block_savepoint(true, 2015012901, 'panopto');
