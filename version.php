d2ea814b694789e1326c75d2d6fb03c3662bc1c0
Panopto, Inc <Panopto@users.noreply.github.com>
2015-11-12 12:08:27 -0800

    Update version number: don't call rolling sync mathods if not supported

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 //Current internal version number.
-$internalversion = 2015110601;
+$internalversion =2015111201;
 
 //Plugin version should normally be the same as the internal version. 
 //If an admin wants to install with an older version number, however, set that here. 
