ac666dd87b59456658ac8297b5adc9e685b10a8c
Panopto, Inc <Panopto@users.noreply.github.com>
2015-12-03 09:58:59 -0800

    Disable max execution time

--- lib/soap_client_with_timeout.php
+++ lib/soap_client_with_timeout.php
@@ -19,6 +19,12 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
+
+//Provisioning operations with many users can take longer than the default 30 seconds to finish executing.
+//We disable maximum execution time for running this script so that it will continue to execute for as long as the
+//timeout duration specified.
+set_time_limit(0);
+
 //This class extends php's built in SoapClient class with logic for handling different timeout durations
 //for making calls. The default timeout is 60 seconds, but can be changed by including a 'timeout' value
 //in the options array passed in as an argument.
