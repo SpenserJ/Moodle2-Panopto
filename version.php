dac2f6e16993462fd8283d234b80168df0ef5def
Panopto, Inc <Panopto@users.noreply.github.com>
2016-02-01 13:16:27 -0800

    Fix warning on version.php

--- version.php
+++ version.php
@@ -23,7 +23,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 //Current internal version number.
-$internalversion =2016012701;
+$internalversion =2016020101;
+
+//Initialize $plugin object if it hasn't been already
+$plugin = (isset($plugin) ? $plugin : new stdClass());
 
 //Plugin version should normally be the same as the internal version. 
 //If an admin wants to install with an older version number, however, set that here. 
